f850e03216bfed3ee44df30507338ad2566bbfbf
Ryan Smith <ryan.smith.p@gmail.com>
2018-03-06 09:02:06 -0600

    Trying displayWidth

--- javascripts/SmartRockets/sketch.js
+++ javascripts/SmartRockets/sketch.js
@@ -22,7 +22,7 @@ var rw = 400;
 var rh = 10;
 
 function setup() {
-  var myCanvas = createCanvas(400, 300);
+  var myCanvas = createCanvas(displayWidth / 1.5 , displayHeight / 4);
   myCanvas.parent('sketch');
   population = new Population();
   lifeP = createP();
